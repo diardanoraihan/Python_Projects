40a56fbca002c687200701d45bc83bbaab05becd
Diardano Raihan <diardano@gmail.com>
2024-06-10 01:58:48 +0700

    feat: add bmi calculator app

---
+++ Reminder/3_ifstatements/bmi_calculator.py
@@ -0,0 +1,20 @@
+# Level: Easy
+
+def bmi_calculator(height, weight):
+  result = weight / (height**2)
+  return result
+
+height = float(input("Enter your height in meters e.g., 1.73"))
+weight = int(input("Enter your weight in Kg e.g., 70"))
+bmi = bmi_calculator(height, weight)
+
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
---
+++ Reminder/3_ifstatements/learning_material.py
@@ -0,0 +1,27 @@
+# Regular If Statement
+def numberChecker(n):
+  if n % 2 == 0:
+    print('Your number is even!')
+  else:
+    print('Your number is odd!')
+
+inputNumber = int(input('Check if your number is even or odd: '))
+numberChecker(inputNumber)
+
+# Nested If Statement
+height = int(input("Please input your height: "))
+if height < 120:
+  print("Yes, you can ride!")
+  age = int(input("Please input your age: "))
+  if age < 12:
+    print("Please pay $5")
+  elif age <= 18:
+    print("Please pay $7")
+  else:
+    print("Please pay $12")
+else:
+  print("Oh no, you can't ride.")
+
+
+
+
